c98741f3fc92db6bd43d52a7e786598bb79e94b3
Amir <amirrasooli69@gmail.com>
2020-04-06 22:29:30 +0430

    Add FormAddUnit

---
+++ Service/FormAddUnit.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Service
+{
+    public partial class FormAddUnit : Form
+    {
+        public FormAddUnit()
+        {
+            InitializeComponent();
+        }
+
+        private void toolExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using(var context=new StimulsoftEntities())
+            {
+                if(context.Unit.Count()>0)
+                {
+                    bool existUnit = context.Unit.Where(c => c.Name == txtUnitName.Text).Any();
+                    if(existUnit)
+                    {
+                        MessageBox.Show("این واحد وجود دارد", "واحد", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    else
+                    {
+                        Unit newUnit = new Unit();
+                        newUnit.Name = txtUnitName.Text;
+                        context.Unit.Add(newUnit);
+                        context.SaveChanges();
+                        MessageBox.Show("واحد ثبت شد", "ثبت", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        txtUnitName.Text = "";
+                        txtUnitName.Focus();
+
+                    }
+
+                }
+                else
+                {
+                    Unit newUnit = new Unit();
+                    newUnit.Name = txtUnitName.Text;
+                    context.Unit.Add(newUnit);
+                    context.SaveChanges();
+                    MessageBox.Show("واحد ثبت شد", "ثبت", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtUnitName.Text = "";
+                    txtUnitName.Focus();
+                }
+
+            }
+        }
+
+        private void FormAddUnit_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Service/FormAddUnit.designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Service
+{
+    partial class FormAddUnit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAddUnit));
+            this.txtUnitName = new System.Windows.Forms.TextBox();
+            this.label28 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtUnitName
+            // 
+            this.txtUnitName.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUnitName.Location = new System.Drawing.Point(71, 28);
+            this.txtUnitName.Name = "txtUnitName";
+            this.txtUnitName.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtUnitName.Size = new System.Drawing.Size(138, 22);
+            this.txtUnitName.TabIndex = 16;
+            // 
+            // label28
+            // 
+            this.label28.AutoSize = true;
+            this.label28.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label28.Location = new System.Drawing.Point(214, 33);
+            this.label28.Name = "label28";
+            this.label28.Size = new System.Drawing.Size(48, 14);
+            this.label28.TabIndex = 17;
+            this.label28.Text = "نام واحد";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolExit});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.menuStrip1.Size = new System.Drawing.Size(266, 24);
+            this.menuStrip1.TabIndex = 18;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolExit
+            // 
+            this.toolExit.Image = ((System.Drawing.Image)(resources.GetObject("toolExit.Image")));
+            this.toolExit.Name = "toolExit";
+            this.toolExit.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.toolExit.Size = new System.Drawing.Size(28, 20);
+            this.toolExit.Click += new System.EventHandler(this.toolExit_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(8, 27);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(57, 23);
+            this.btnSave.TabIndex = 19;
+            this.btnSave.Text = "ثبت  ";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // FormAddUnit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 64);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtUnitName);
+            this.Controls.Add(this.label28);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormAddUnit";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormAddUnit";
+            this.Load += new System.EventHandler(this.FormAddUnit_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUnitName;
+        private System.Windows.Forms.Label label28;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolExit;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
